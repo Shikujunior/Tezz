f2deed74b573f0181db31b3683579b93c0a142ea
Shikujunior <137788930+Shikujunior@users.noreply.github.com>
2023-07-08 11:36:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ let firstName ="Techla";
 let secondName ="college";
 let user=firstName+ " "+ secondName;
 console.log(user);
+
+let a=math.floor(Math.random () *10)
+let b=math.floor(Math.random () *10)
+let c=math.floor(Math.random () *10)
+Console.log(a)
+Console.log(b)
+Console.log(c)
